a3023001c47f1256b8da3e181a74f7ad35f476af
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-11-05 23:14:40 +0100

    MOdifying some text in commands file

--- commands.sh
+++ commands.sh
@@ -7,6 +7,6 @@ sed 's/gili/world' - 	  		# Taking input from the stdin
 cat text | sed 's/gili/world'   	# Taking input from the pipeline and write to the stdout
 cat text | sed 's/gili/world' text	# Taking input from the pipleline and write to the text file
 
-sed 's/gili/world/w other' text
-
+sed -i 's/gili/world/' text		# Taking the input from the file text and write to the same file
+sed 's/gili/world/w other' text		# Taking the input from the file text and write to the other file
 
