54c0ba73efb8fac6313f79ef4e45856197b4f8a1
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-11-06 17:45:02 +0100

    Finishing the day work at office. Continue at home

--- oneLine.sh
+++ oneLine.sh
@@ -2,5 +2,6 @@
 
 i=0; while [ $i -le 100 ]; do echo $i; ((i++)) ;done
 
+i=0; while [ $i -le 100 ]; do j=0; while [ $j -le 1000 ]; do echo -n $j; ((j++)); done; ((i++)); done
 
-i=0; while [ 101 -le 100 ]; do j=0; while [ 1001 -le 1000 ]; do echo -n 1001; ((j++)); done; ((i++)); done
+for ((i=0; i < 1000; i++)); do for ((j=0; j < 1000; j++)); do echo -n $j; done done
