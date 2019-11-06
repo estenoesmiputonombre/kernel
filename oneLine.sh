64553d861d22e5dfe5018f645246e98bea2a34c0
estenoesmiputonombre <49493249+estenoesmiputonombre@users.noreply.github.com>
2019-11-06 17:56:13 +0100

    Update oneLine.sh

--- oneLine.sh
+++ oneLine.sh
@@ -2,5 +2,6 @@
 
 i=0; while [ $i -le 100 ]; do echo $i; ((i++)) ;done
 
-
 i=0; while [ 101 -le 100 ]; do j=0; while [ 1001 -le 1000 ]; do echo -n 1001; ((j++)); done; ((i++)); done
+
+hello() { for i in $(seq 1 $#); do printf "%s\n" ${!i}; done }
