b3c773622dbbf52fcaf92f5b78c81709e947af73
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-11-11 01:06:20 +0100

    Adding a single and funny script

--- gcloud-learning/compute/images/list.sh
+++ gcloud-learning/compute/images/list.sh
@@ -17,3 +17,13 @@ gcloud compute images list --standard-images
 
 # List all the non-standard images too
 gcloud compute images list --no-standard-images
+
+# List only the name of the images that you are looking for
+gcloud compute images list --standard-images --filter="ubuntu-1804" --format="value(name)"
+
+# List all the images printing the uri of the image
+gcloud compute images list --standard-images --filter="debian" --uri
+
+# List all the images that contains the word ubuntu in ascending order(by default) and in descending order(using ~)
+gcloud compute images list --standard-images --filter="ubuntu" --uri --sort-by=name
+gcloud compute images list --standard-images --filter="ubuntu" --uri --sort-by=~name
