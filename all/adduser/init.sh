20c7edae2b17db518e985b13d8d54ec6563bbf6e
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-11-19 22:59:34 +0000

    Addinf some commands in the shell file

--- all/adduser/init.sh
+++ all/adduser/init.sh
@@ -9,4 +9,12 @@ adduser --uid 1003 user2
 
 adduser --firstuid 1000 --lastuid 2000 user3
 
+# We can create a user and add it to a group by id or name
+adduser --gid 1000 user4
+
+adduser --ingroup group user5
+
+# We can add extra groups to a user
+adduser --add_extra_groups group user6
+
 # 
